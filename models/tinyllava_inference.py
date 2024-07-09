90924f89015aa0d345d9553e5764cf8327469c60
- <->
2024-07-09 12:30:05 +0500

    added tinyllava_inference.py

---
+++ models/tinyllava_inference.py
@@ -0,0 +1,30 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+from PIL import Image
+
+from models.base import Mllm
+
+
+class Llava(Mllm):
+
+    def __init__(self, model_name_or_path, **kwargs):
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_name_or_path, torch_dtype=torch.float16, trust_remote_code=True
+        )
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(self.device)
+        self.config = self.model.config
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_name_or_path,
+            use_fast=False,
+            model_max_length=self.config.tokenizer_model_max_length,
+            padding_side=self.config.tokenizer_padding_side,
+        )
+
+    def evaluate(self, prompt, filepath):
+        image = Image.open(filepath)
+        output_text, genertaion_time = self.model.chat(
+            prompt=prompt, image=image, tokenizer=self.tokenizer
+        )
+        return output_text
