acb076a70846ae2139fa25387fd6070e8f249313
- <->
2024-07-09 12:49:01 +0500

    relocated eval.sh

--- eval.sh
+++ eval.sh
@@ -1,10 +1,12 @@
-model="mplug1"
+MODEL_PATH="$1"
+MODEL_NAME="$2"
+
 
 categories=("privacy" "bias" "toxicity" "non-exisitent" "position-swapping" "noise-injection" "legality")
 
 for category in "${categories[@]}"
 do
-    save_path="results/${category}_${model}.jsonl"
+    save_path="results/${category}_${MODEL_NAME}.jsonl"
 
     # 检查 save_path 是否已存在
     if [ -f "$save_path" ]; then
@@ -12,8 +14,8 @@ do
         continue
     fi
 
-    python evaluate.py --model {YOUR MODEL PATH} \
+    python evaluate.py --model $MODEL_PATH \
                     --save_path "$save_path" \
                     --data_path "data/${category}" \
-                    --log_file "logs/evaluate-${category}_${model}.log" \
+                    --log_file "logs/evaluate-${category}_${MODEL_NAME}.log" \
 done
